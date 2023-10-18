e85e33e2404a310a3dc377e8f527acd10a21f784
romainremyb <romain.bourgeois.22.n1@gmail.com>
2023-10-18 18:12:19 +0200

    g

--- src/cimple_querying.py
+++ src/cimple_querying.py
@@ -7,7 +7,6 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 
 class localGraph:
-  # index types by url or class statements
 
   def __init__(self, folderpath, filenames=['cimple-ontology.ttl', 'cimple-vocabularies.ttl', 'claim-review.ttl',
                                             'mediaeval.ttl', 'propaganda.ttl', 'check-that.ttl', 'birdwatch.ttl',
@@ -20,9 +19,12 @@ class localGraph:
         print(f"File {filename} not found in {folderpath}!", file=sys.stderr)
         return
     
-    # add namespace
-    cimpler = Namespace("http://data.cimple.eu/")
+    cimpler = Namespace("http://data.cimple.eu/") # add this missing namespace
     self.g.bind("cimple_records", cimpler) 
+    tweet = Namespace("http://data.cimple.eu/tweet/") # add this missing namespace
+    self.g.bind("tweet", tweet) 
+    news = Namespace("http://data.cimple.eu/news-article/") # add this missing namespace
+    self.g.bind("news", news) 
 
     self.prefix_namespaces=defaultdict()
     self.namespaces_prefix=defaultdict()
@@ -31,7 +33,7 @@ class localGraph:
         self.namespaces_prefix[uri]=prefix
     
     
-  def get_full_namespace_dict(self, return_dicts = True):
+  def get_full_namespace_dict(self, return_unique_dict=True, return_dicts = True):
     self.uris_in_namespaces = defaultdict(lambda: defaultdict(list))
     triples = self.g.triples((None, None, None))
     for s in triples:
@@ -42,23 +44,54 @@ class localGraph:
           self.uris_in_namespaces[k]['predicates'].append(s[1])
         if str(k) in str(s[2]):
           self.uris_in_namespaces[k]['objects'].append(s[2])
+    # dictionary with unique URIs
+    if return_unique_dict==True:
+      self.unique_uris_in_namespaces = defaultdict(lambda: defaultdict(list))
+      for pkey in self.uris_in_namespaces.keys():
+        for skey in self.uris_in_namespaces[pkey].keys():
+          self.unique_uris_in_namespaces[pkey][skey]=list(set(self.uris_in_namespaces[pkey][skey]))
 
     if return_dicts==True:
-      return self.uris_in_namespaces
-    #TODO save dict with unique uris
+      if return_unique_dict==True:
+        return self.uris_in_namespaces, self.unique_uris_in_namespaces
+      else:
+        return self.uris_in_namespaces
+    
 
-  #def records_from_type(self):
+  def get_unique_uris_from_type_statement(self, type_statement):
+    """
+    retrieves unique URIs defined as type of type_statement
+    
+    : param type_statement: URIRef object. ex: self.prefix_namespaces[prefix] + object (e.g. NewsArticle, with namespace: http://schema.org/)
+    """
+    triples=self.g.triples((None, URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"), type_statement))
+    return set([t[0] for t in triples]) # unique subjects
+      
+
+  def get_unique_uris_from_type_namespace(self, uri_namespace, only_as_subjects=False):
+    """
+    retrieves unique URIs yielding the uri_namespace. 
+
+    : param uri_namespace: URIRef object. namespace of record (e.g. http://data.cimple.eu/news-article/)
+    : param only_as_subjects: if True, will only retrieve the records defined as subjects in triples
+    """
+    triples=self.g.triples((None, None, None))
+    uris=[]
+    for s in triples:
+      if str(uri_namespace) in str(s[0]):
+        uris.append(s[0])
+      if only_as_subjects==True:
+        if str(uri_namespace) in str(s[2]):
+          uris.append(s[2])
+    return set(uris)
+
 
   def all_unique_predicates(self): # check if all predicates in prefix_map
     """
     all unique predicates
     """
     triples=self.g.triples((None, None, None))
-    all_preds=set()
-    for s in triples:
-      if s[1] not in all_preds:
-        all_preds.add(s[1])
-    return all_preds
+    return set([s[1] for s in triples])
 
     
   def get_unique_predicates_for_recordType(self, recordtype, as_subject=True): 
@@ -82,9 +115,8 @@ class localGraph:
     """
     retieve all statements given a predicate
     
-    : param predicate:  -> self.prefix_namespaces[prefix] + predicate (e.g. NewsArticle)
+    : param predicate:  -> ex: URIRef('http://schema.org/articleBody')
     """
-
     triples = self.g.triples((None, URIRef(predicate), None))
     data = dict()
     for s in triples:
@@ -97,16 +129,25 @@ class localGraph:
     retieves all statements given a predicate and a subject/object as recordType
     """
     triples = self.g.triples((None, URIRef(predicate), None))
+    type_uris = self.get_unique_uris_from_type_statement(recordType) #NOTE: could use other function
     data = dict()
-    for s in triples: 
-      #TODO check subject/object record from type: namespace or statement based -> check all records with namespaces are defined as instances
-      data[s[0]]=s[2]
+    if subject==True:
+      for s in triples: 
+        if s[0] in type_uris:
+          data[s[0]]=s[2]
+    else:
+      for s in triples: 
+        if s[2] in type_uris:
+          data[s[0]]=s[2]
     return data
 
 
 
 
-###
+
+
+
+###______________________________________OLD_SPARQL_requests
 
 def build_SPARQL_wrapper(endpoint_url):
   sparql = SPARQLWrapper(
