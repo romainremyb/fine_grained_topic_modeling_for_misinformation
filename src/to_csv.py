041d5bea5b94df5ea87f4099a320bee017ad6e43
romainremyb <romain.bourgeois.22.n1@gmail.com>
2023-10-24 15:31:00 +0200

    preprocessing

--- src/to_csv.py
+++ src/to_csv.py
@@ -10,7 +10,7 @@ cimple = localGraph('KGs_v6/')
 
 uris_in_namespaces, unique_uris_in_namespaces = cimple.get_full_namespace_dict()
 
-os.chdir(os.path.expanduser("~")+'/Desktop/topic_modeling/fine_grained_topic_modeling_for_misinformation/ToMODAPI_data/')
+os.chdir(os.path.expanduser("~")+'/Desktop/topic_modeling/fine_grained_topic_modeling_for_misinformation/data/')
 
 type_statements = cimple.get_all_triples_from_predicate(URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'))
 predicates_per_types = defaultdict(dict)
@@ -19,28 +19,19 @@ for i in set(type_statements.values()):
     predicates_per_types[i]['as_objects']=cimple.get_unique_predicates_for_recordType(i, as_subject=False)
 
 
-twitts = cimple.get_all_triples_from_predicate_subjectobject(URIRef('http://schema.org/text'), URIRef('http://schema.org/SocialMediaPosting'), subject=True)
-twitter_data = dict()
-twitter_data['id_doc']=[]
-twitter_data['content']=[]
-for uri in twitts.keys():
-    twitter_data['id_doc'].append(str(uri).rsplit('/', 1)[-1])
-    twitter_data['content'].append(str(twitts[uri]))
+data = dict()
+data['id_doc']=[]
+data['content']=[]
+for pair in [(URIRef('http://schema.org/text'), URIRef('http://schema.org/SocialMediaPosting')), 
+              (URIRef('http://schema.org/text'), URIRef('http://schema.org/Claim'))]:
+    statements = cimple.get_all_triples_from_predicate_subjectobject(pair[0], pair[1], subject=True)
+    for uri in statements.keys():
+        data['id_doc'].append(str(uri).rsplit('/', 1)[-1])
+        data['content'].append(str(statements[uri]))
 
-with open(str(os.getcwd()).rsplit('/', 1)[0]+'/data/twitts.csv', 'w', newline='') as file:
+
+with open(str(os.getcwd()).rsplit('/', 1)[0]+'/data/data.csv', 'w', newline='') as file:
     writer = csv.writer(file, quoting=csv.QUOTE_ALL, quotechar='"')
-    writer.writerow(twitter_data.keys())
-    writer.writerows(zip(*twitter_data.values()))
+    writer.writerow(data.keys())
+    writer.writerows(zip(*data.values()))
 
-afp = cimple.get_all_triples_from_predicate_subjectobject(URIRef('http://schema.org/articleBody'), URIRef('http://schema.org/NewsArticle'), subject=True)
-afp_data = dict()
-afp_data['id_doc']=[]
-afp_data['content']=[]
-for uri in afp.keys():
-    afp_data['id_doc'].append(str(uri).rsplit('/', 1)[-1])
-    afp_data['content'].append(str(afp[uri]))
-
-with open(str(os.getcwd()).rsplit('/', 1)[0]+'/data/afp.csv', 'w', newline='') as file:
-    writer = csv.writer(file, quoting=csv.QUOTE_ALL, quotechar='"')
-    writer.writerow(afp_data.keys())
-    writer.writerows(zip(*afp_data.values()))
--- src/utils.py
+++ src/utils.py
@@ -1,26 +1,58 @@
 import re
 import nltk
 from nltk import pos_tag
+import sys
+import spacy
 
 
 init_done = False
+
+unicodes_to_remove = ['\\u2015', '\\u2033', '\\u2063', '\\u0627',
+ '\\u2066', '\\u2069', '\\u200d', '\\u2013', '\\ufe0f', '\\u2026',
+ '\\u2018', '\\u2014', '\\u2022', '\\u201d', '\\u201c', '\\u2019']
 
 
 def is_list_of_strings(lst):
     return bool(lst) and isinstance(lst, list) and all(isinstance(elem, str) for elem in lst)
 
 
-def input_to_list_string(data, preprocessing=False):
+def preprocess_for_latent_distribs(data, preprocessing=False, return_idxs=False):
     if type(data) == str:
-        with open(data, "r", encoding='utf-8') as datafile:
-            text = [line.rstrip() for line in datafile if line]
+        try:
+            if data[-4:]=='.csv':
+                with open(data, "r", encoding='utf-8') as datafile:
+                    text, idxs = [], []
+                    for line in datafile:
+                        if len(line.rstrip().split('",',1)[-1][1:-1])>=3:
+                            text.append(line.rstrip().split('",',1)[-1][1:-1])
+                            idxs.append(line.rstrip().split('",',1)[0][1:-1])
+            elif data[-4:]=='.txt':
+                with open(data, "r", encoding='utf-8') as datafile:
+                    text = [line.rstrip() for line in datafile if line and len(line)!=0]
+            else:
+                print('file neither .csv nor .txt')
+                return
+
+        except FileNotFoundError as e:
+            print(f"path {data} not found !", file=sys.stderr)
+            return
+
     elif is_list_of_strings(data):
         text = data
     else:
         raise ValueError('data should be a path or a list of strings')
+    
     if preprocessing:
         text = [preprocess(doc) for doc in text]
-    return text
+    
+    if return_idxs==True:
+        try:
+            return text[1:], idxs[1:]
+        except NameError:
+            print('idxs list not defined')
+    else:
+        return text[1:]
+
 
 
 def _init():
@@ -31,17 +63,18 @@ def _init():
     init_done = True
 
 
-def preprocess(text, strip_brackets=False):
-    """ Preprocessing function for text, consisting of:
-    - Removing numbers, which, in general, do not contribute to the broad semantics;
-    - Removing the punctuation and lower-casing the text;
-    - Removing the standard English stop words;
-    - Lemmatisation using Wordnet, in order to deal with inflected forms as they are a single semantic item;
-    - Ignoring words with 2 letters or less. In facts, they are mainly residuals from removing punctuation
-            e.g. stripping punctuation from _people’s_ produces _people_ and _s_
+def preprocess(text, strip_brackets=False, keep_unicodes=True, add_adj_nn_pairs=True):
+    """ 
+    * unicodes are extracted before applying stop words and lemmatization
+    * !keep?! keep numbers that are attached to words or other textual characters ex: keep 19 in covid19 not in covid 19
 
     :param str text: the text to be preprocessed
     :param bool strip_brackets: If True, the content inside brackets is excluded
+    :param bool keep_unicodes: extract unicodes and aprend them to final output
+        NOTE: removes unicodes from unicodes_to_remove. Might wanna extract unocodes from a given list only
+    :param bool add_adj_nn_pairs: extract adjective/noun pairs. Append them to result
+        NOTE: this implementation keeps adjs and nouns as sole items in final list
+              could be implemented with either nltk or spacy
     """
     global init_done
 
@@ -50,14 +83,68 @@ def preprocess(text, strip_brackets=False):
 
     if strip_brackets:
         text = re.sub(r'\((.*?)\)', ' ', text)
-    text = re.sub(r'\d+', '', text)
-    text = nltk.tokenize.RegexpTokenizer(r'\w+').tokenize(text.lower())
-    text = [w for w in text if w not in nltk.corpus.stopwords.words('english')]
+
+    if keep_unicodes:
+        pattern1 = r'\\U[0-9A-Za-z]{8}'
+        pattern2 = r'\\u[0-9A-Za-z]{4}'
+        unicodes1 = re.findall(pattern1, text.encode('unicode-escape').decode())
+        unicodes2 = re.findall(pattern2, text.encode('unicode-escape').decode())
+        unicodes = [u for u in unicodes1 if u not in unicodes_to_remove]
+        unicodes.extend([u for u in unicodes2 if u not in unicodes_to_remove])
+    
+    text = re.sub(r'\b\d+\b', '', text) #or all digits ex: r'\d+'
+
+    text = nltk.tokenize.RegexpTokenizer(r'\w+').tokenize(text.lower()) # tokenize space seperated group of characters
 
     lem = nltk.stem.WordNetLemmatizer()
 
-    text = [w for w in text if len(w) >= 3]
-    text = [lem.lemmatize(i, j[0].lower()) if j[0].lower() in ['a', 'n', 'v']
-            else lem.lemmatize(i) for i, j in pos_tag(text)]
+    if add_JJ_NN_pairs_nltk:
+        adj_noun_pairs = add_JJ_NN_pairs_nltk(text, lem) #merge adjectives ?
+
+    text = pos_and_lemma(text, lem, adjectives=False)
+
+    text = [w for w in text if w not in nltk.corpus.stopwords.words('english')]
+
+    text = [w for w in text if len(w) >= 3] #ignore with two letters or less
+
+    if add_JJ_NN_pairs_nltk:
+        text.extend(adj_noun_pairs)
+
+    if keep_unicodes:
+        text.extend(unicodes)
+    
     text = ' '.join(text)
     return text
+
+
+def pos_and_lemma(tokens, lem, adjectives=False):
+    # nltk pos tags: https://www.guru99.com/pos-tagging-chunking-nltk.html
+    # lem.lemmatize(i,j) -> j: POS tag. Valid options are `"n"` for nouns,`"v"` for verbs, `"a"` for adjectives, `"r"` for adverbs and `"s"` for satellite adjectives.
+    result=[]
+    for i, j in pos_tag(tokens):
+        if j[0].lower() in ['n','v']:
+            result.append(lem.lemmatize(i, j[0].lower()))
+        if adjectives==True and j[0].lower()=='j':
+            result.append(lem.lemmatize(i, 'a'))
+    return result
+
+
+def add_JJ_NN_pairs_nltk(tokens, lem):
+    adj_noun_pairs = []
+    pos_tagged = pos_tag(tokens)
+    for i in range(len(pos_tagged) - 1):
+        word, pos = pos_tagged[i]
+        next_word, next_pos = pos_tagged[i + 1]
+        if pos.startswith('JJ') and next_pos.startswith('NN'):
+            adj_noun_pairs.append(lem.lemmatize(word,'a') + next_word)
+    return adj_noun_pairs
+
+
+def add_JJ_NN_pairs_spacy(text, lem):
+    adj_noun_pairs = []
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    for token in doc:
+        if token.pos_ == "ADJ" and token.head.pos_ == "NOUN":
+            adj_noun_pairs.append(token.text + ' ' + token.head.text)
+    return adj_noun_pairs
